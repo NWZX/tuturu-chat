3c24c1183186449cd3a24e1a83553da4c53d15d0
NWZX <nwzx@users.noreply.github.com>
2020-11-30 19:54:32 -0400

    gh-pages

---
+++ precache-manifest.12b7f0355ae637fb58239d1c4ba1d277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74034520e21f6fb9d537",
+    "url": "/tuturu-chat/css/chunk-vendors.1891c732.css"
+  },
+  {
+    "revision": "3b999199cde035054f97563f069b3ced",
+    "url": "/tuturu-chat/index.html"
+  },
+  {
+    "revision": "ca406cf53da49ec8a00c",
+    "url": "/tuturu-chat/js/app.165102b1.js"
+  },
+  {
+    "revision": "74034520e21f6fb9d537",
+    "url": "/tuturu-chat/js/chunk-vendors.5d5e8ec1.js"
+  },
+  {
+    "revision": "4316a215ff4dc881d6e57d772ba25bcd",
+    "url": "/tuturu-chat/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tuturu-chat/robots.txt"
+  }
+]);
